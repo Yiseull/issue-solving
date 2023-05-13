f126daebcca89f7a5b7050a18bbbd816a51bb6f4
Yiseull <omjl5123@catholic.ac.kr>
2023-05-14 04:03:41 +0900

    :white_check_mark: N+1 발생 테스트 - FetchType.EAGER 수정

--- src/test/java/com/spring/issuesolving/JpaNPlus1Test.java
+++ src/test/java/com/spring/issuesolving/JpaNPlus1Test.java
@@ -5,12 +5,12 @@ import com.spring.issuesolving.JpaNPlus1.ArtistRepository;
 import com.spring.issuesolving.JpaNPlus1.Song;
 import com.spring.issuesolving.JpaNPlus1.SongRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,20 +24,29 @@ public class JpaNPlus1Test {
     @Autowired
     private SongRepository songRepository;
 
+    @BeforeEach
+    public void setup() {
+
+        // given
+        Artist artist1 = new Artist("뉴진스");
+        artistRepository.save(artist1);
+
+        Artist artist2 = new Artist("Maroon 5");
+        artistRepository.save(artist2);
+
+        Song song1 = Song.builder().title("Hype boy").artist(artist1).build();
+        songRepository.save(song1);
+
+        Song song2 = Song.builder().title("Attention").artist(artist1).build();
+        songRepository.save(song2);
+
+        Song song3 = Song.builder().title("Memories").artist(artist2).build();
+        songRepository.save(song3);
+    }
+
     @DisplayName("CASE 1. FetchType.EAGER")
     @Test
     void occurJpaNPlus1_EAGER() {
-        // given
-        Artist newJeans = new Artist("뉴진스");
-        Artist maroon5 = new Artist("Maroon 5");
-
-        Song song1 = Song.builder().title("Hype boy").artist(newJeans).build();
-        Song song2 = Song.builder().title("Attention").artist(newJeans).build();
-        Song song3 = Song.builder().title("OMG").artist(newJeans).build();
-        Song song4 = Song.builder().title("Memories").artist(maroon5).build();
-
-        artistRepository.saveAll(Arrays.asList(newJeans, maroon5));
-        songRepository.saveAll(Arrays.asList(song1, song2, song3, song4));
 
         // when - JPA N+1 문제 발생: 조회는 1번 했지만 조회 쿼리는 3번 실행
         List<Artist> artists = artistRepository.findAll();
